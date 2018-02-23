39a1f5a4911dc826701d263a42be815ecbd4ccaf
Rafael Zarate <rafazrte@gmail.com>
2018-02-22 19:00:14 -0800

    Solving bug with null params

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: rzarate <rzarate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/19 18:24:00 by rzarate           #+#    #+#             */
-/*   Updated: 2018/02/19 20:19:34 by rzarate          ###   ########.fr       */
+/*   Updated: 2018/02/22 18:53:11 by rzarate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	int i;
 
 	i = 0;
+	if (!s1 || !s2)
+		return (0);
 	while ((s1[i] || s2[i]) && i < (int)n)
 	{
 		if (s1[i] != s2[i])
